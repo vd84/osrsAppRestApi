8bcfa9fee803fe4cad98bd48b833c8462e658df8
Douglas Hammarstam <dogge20@hotmail.com>
2019-08-15 19:20:35 +0200

    fdfd

---
+++ src/main/java/com/douglashammarstam/plantAppRestAPI/PlantAppRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.douglashammarstam.plantAppRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantAppRestApiApplication.class, args);
+	}
+
+}
