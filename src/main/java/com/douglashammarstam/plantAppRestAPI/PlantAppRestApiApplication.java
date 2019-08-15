b4327f2e63d37b67fdd3fe339021b4b2b207c1c9
Douglas Hammarstam <dogge20@hotmail.com>
2019-08-15 19:45:55 +0200

    fdfd

--- src/main/java/com/douglashammarstam/plantAppRestAPI/PlantAppRestApiApplication.java
+++ src/main/java/com/douglashammarstam/plantAppRestAPI/PlantAppRestApiApplication.java
@@ -2,9 +2,19 @@ package com.douglashammarstam.plantAppRestAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class PlantAppRestApiApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlantAppRestApiApplication.class, args);
